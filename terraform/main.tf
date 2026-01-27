1c3c7920f88daad204236251c71063305be5feef
marlabharghavsai <marlabharghavsai@gmail.com>
2026-01-27 05:04:03 +0000

    Add IAM role and least-privilege policy for Lambda functions

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,11 +32,94 @@ resource "aws_s3_bucket_lifecycle_configuration" "uploads_lifecycle" {
 ############################################
 
 resource "aws_sqs_queue" "image_processing_requests" {
-  name                      = "${var.app_name}-image-processing-requests"
+  name                       = "${var.app_name}-image-processing-requests"
   visibility_timeout_seconds = 300
 }
 
 resource "aws_sqs_queue" "image_processing_results" {
-  name                      = "${var.app_name}-image-processing-results"
+  name                       = "${var.app_name}-image-processing-results"
   visibility_timeout_seconds = 300
+}
+
+############################################
+# IAM Role for Lambda Functions
+############################################
+
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.app_name}-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+############################################
+# IAM Policy (Least Privilege)
+############################################
+
+resource "aws_iam_policy" "lambda_policy" {
+  name = "${var.app_name}-lambda-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+
+      # CloudWatch Logs
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      },
+
+      # S3 Access
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ]
+        Resource = [
+          "${aws_s3_bucket.uploads.arn}/*",
+          "${aws_s3_bucket.processed.arn}/*"
+        ]
+      },
+
+      # SQS Access
+      {
+        Effect = "Allow"
+        Action = [
+          "sqs:SendMessage",
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueAttributes"
+        ]
+        Resource = [
+          aws_sqs_queue.image_processing_requests.arn,
+          aws_sqs_queue.image_processing_results.arn
+        ]
+      }
+    ]
+  })
+}
+
+############################################
+# Attach Policy to Role
+############################################
+
+resource "aws_iam_role_policy_attachment" "lambda_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
 }
